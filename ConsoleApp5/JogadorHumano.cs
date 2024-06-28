7493c53f84d35cbc2c997cf74d6b61f6724b5d6e
Diogo146 <166730973+Diogo146@users.noreply.github.com>
2024-06-28 18:31:07 -0300

    Update JogadorHumano.cs

--- ConsoleApp5/JogadorHumano.cs
+++ ConsoleApp5/JogadorHumano.cs
@@ -39,33 +39,33 @@ namespace ConsoleApp5
         
         public void GerarNickName(string nomeCompleto)
         {
-           string name,parte;
-string[] palavra;
-string usuario;
-string iniciais = "";
-char inicial;
-
-Console.WriteLine("Insira seu nome completo:");
-name = Console.ReadLine();
-name = name.ToUpper();
-
-palavra = name.Split(' ');
-
-           
-usuario = palavra[0];
+             string parte;
+  string[] palavra;
+  string usuario;
+  string iniciais = "";
+  char inicial;
 
 
-for (int i = 1; i < palavra.Length; i++)
-{
-    parte = palavra[i];
-    inicial = parte[0];
-    iniciais += inicial;
-}
+ nomeCompleto = nomeCompleto.ToUpper();
+
+palavra = nomeCompleto.Split(' ');
+
+  
+     usuario = palavra[0];
 
 
-string nickname = usuario + iniciais;
+      for (int i = 1; i < palavra.Length; i++)
+    {
+     parte = palavra[i];
+     inicial = parte[0];
+     iniciais += inicial;
+    }
 
-Console.WriteLine("Seu nickname é: " + nickname);
+
+         string nickname = usuario + iniciais;
+
+     Console.WriteLine("Seu nickname é: " + nickname);
+  
  //este método deve receber o nome completo do jogador como parâmetro (parâmetro do método: string) e deve gerar o seu nickname.
 
         }
